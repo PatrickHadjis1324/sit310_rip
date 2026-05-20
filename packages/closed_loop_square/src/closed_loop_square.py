7ec82ea22451f489e18bacc79e13aded46cd5b1a
S4_RIP <s222236157@deakin.edu.au>
2026-05-20 13:14:36 +1000

    Task 4.2C done

--- packages/closed_loop_square/src/closed_loop_square.py
+++ packages/closed_loop_square/src/closed_loop_square.py
@@ -2,6 +2,7 @@
 
 import rospy
 import math
+from duckietown_msgs.msg import WheelEncoderStamped
 from duckietown_msgs.msg import Twist2DStamped
 from duckietown_msgs.msg import FSMState
 
@@ -24,23 +25,25 @@ class ClosedLoopSquareNode:
 
         rospy.Subscriber(
             "/mybota002409/left_wheel_encoder_node/tick",
-            EncoderMsgType,
+            WheelEncoderStamped,
             self.left_encoder_callback,
             queue_size=1,
         )
         rospy.Subscriber(
             "/mybota002409/right_wheel_encoder_node/tick",
-            EncoderMsgType,
+            WheelEncoderStamped,
             self.right_encoder_callback,
             queue_size=1,
         )
 
     def left_encoder_callback(self, msg):
         # Process left encoder data if needed
+        rospy.loginfo(f"Left encoder callback: {msg.data}")
         self.left_encoder_data = msg.data
 
     def right_encoder_callback(self, msg):
         # Process right encoder data if needed
+        rospy.loginfo(f"Right encoder callback: {msg.data}")
         self.right_encoder_data = msg.data
 
     def fsm_callback(self, msg):
@@ -48,7 +51,7 @@ class ClosedLoopSquareNode:
         if msg.state == "LANE_FOLLOWING":
             rospy.sleep(1)  # Sleep for a moment to ensure the system is ready
             # Move func
-            self.move_straight(1.0, 0.5)  # Move forward 1 meter at 0.5 m/s
+            self.move_robot()  # Move forward 1 meter at 0.5 m/s
             rospy.sleep(1)  # Sleep for a moment before the next command
 
     def distance_to_ticks(self, distance):
@@ -69,8 +72,12 @@ class ClosedLoopSquareNode:
         start_left = self.left_encoder_data
         start_right = self.right_encoder_data
 
+        slow_down = 0.9
+        min_speed = 0.2
+
+        
         self.cmd_msg.header.stamp = rospy.Time.now()
-        self.cmd_msg.v = 1.0
+        self.cmd_msg.v = speed
         self.cmd_msg.omega = 0.0
         rate = rospy.Rate(10)  # 10 Hz
 
@@ -79,10 +86,64 @@ class ClosedLoopSquareNode:
             left_travel = abs(self.left_encoder_data - start_left)
             right_travel = abs(self.right_encoder_data - start_right)
             average_travel = (left_travel + right_travel) / 2
+
+            if average_travel >= slow_down * ticks_needed:
+                current_speed = min_speed
+            else:
+                current_speed = speed
+
             if average_travel >= ticks_needed:
                 self.stop_robot()
                 break
             rate.sleep()
+        self.stop_robot()
+
+
+    def rotate_in_place(self, angle_deg, omega):
+
+        TICKS_PER_DEG = 0.47
+
+        start_left = self.left_encoder_data
+        start_right = self.right_encoder_data
+
+        target = abs(angle_deg) * TICKS_PER_DEG
+
+        rate = rospy.Rate(10)
+
+        while not rospy.is_shutdown():
+
+            delta_left = abs(self.left_encoder_data - start_left)
+            delta_right = abs(self.right_encoder_data - start_right)
+
+            delta = (delta_left + delta_right) / 2
+
+            if delta >= target:
+                break
+
+            self.cmd_msg.header.stamp = rospy.Time.now()
+            self.cmd_msg.v = 0.0
+            self.cmd_msg.omega = omega
+            self.pub.publish(self.cmd_msg)
+
+            rate.sleep()
+
+    def move_robot(self):
+
+        rospy.loginfo("Starting CLOSED LOOP square")
+
+        for i in range(4):
+
+            rospy.loginfo("Side %d", i + 1)
+
+            # Forward
+            self.move_straight(1.0, 0.3)
+            #test self.move_straight(1.0, 0.6)
+
+            # Rotate
+            self.rotate_in_place(90, 1.5)
+            #test self.rotate_in_place(90, 2.0)
+
+        rospy.loginfo("DONE")
         self.stop_robot()
 
 
@@ -92,3 +153,4 @@ if __name__ == "__main__":
         duckiebot_movement.run()
     except rospy.ROSInterruptException:
         pass
+
